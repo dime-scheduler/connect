3212d2e352a6c69e0bd8d376febe915333bc38d4
Hendrik Bulens <hendrik.bulens@gmail.com>
2021-10-13 13:57:24 -0500

    Import functions #1

---
+++ src/Functions/SendImportRequestFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Dime.Scheduler.Sdk.Import;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Dime.Scheduler.AzureFunctions.Test
+{
+    public class SendImportRequestFunction : ImportService<ImportRequest>
+    {
+        [FunctionName("importRequest")]
+        public Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+            => Import(req, log);
+    }
+}
